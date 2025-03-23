66940ee9c68f86ee5e9007161e1c712f0f3c2db7
Jman23444 <jaxonhicks@gmail.com>
2025-03-22 18:54:18 -0700

    fix: fixing display color of submit button in edit/create modals

--- src/modules/view/components/modals.js
+++ src/modules/view/components/modals.js
@@ -39,7 +39,7 @@ const taskModal = {
             <textarea id="task-description" name="task-description" placeholder="Enter Description" rows="4">${task ? task.description : ''}</textarea>
           </div>
           <button type="submit" class="modal-btn submit-btn">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
               <polyline points="20 6 9 17 4 12"/>
             </svg>
           </button>
